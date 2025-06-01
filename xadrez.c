b6b16f1cd8aa5bef6cf55bc6a4bbe59a2c663ee7
romuloalexandre93 <romuloalexandre93@gmail.com>
2025-06-01 15:50:57 +0000

    Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int i = 0, a1 = 5, a2 = 5, a3 = 8;
+    int i, j;
 
-    printf("### Bem-vindo ao Desafio de Movimento de Xadrez! ###\n");
-
-    printf("\nImprimindo movimento da peça Torre.\n");
-    while(i < a1){
-        printf("Direita\n");
-        i++;
+    //Imprimindo movimento do Cavalo.
+    printf("Cavalo em movimento...\n");
+    for(i = 0; i < 1; i++){
+        printf("Baixo\n");
+        j = 0;
+        while(j < 3){
+            printf("Esquerda\n");
+            j++;
+        }
     }
-
-    printf("\nImprimindo movimento da peça Bispo.\n");
-    i = 0;
-    do{
-        printf("Cima, Direita\n");
-        i++;
-    } while(i < a2);
-
-    printf("\nImprimindo movimento da peça Rainha.\n");
-    for(i = 0; i < a3; i++){
-        printf("Esquerda\n");
-    }
+    printf("\nMovimento finalizado.\n");
 
     return 0;
 }
